7b40d2eac00fe9508d708fc05021d1d6ff5fae6f
MWNEW-928 <marlonmartiosp12@gmail.com>
2025-10-21 10:29:04 -0500

    inclusion del form

--- GUI/FrmIA.Designer.cs
+++ GUI/FrmIA.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtHistorial = new System.Windows.Forms.RichTextBox();
+            this.txtPregunta = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHistorial
+            // 
+            this.txtHistorial.Location = new System.Drawing.Point(12, 12);
+            this.txtHistorial.Name = "txtHistorial";
+            this.txtHistorial.Size = new System.Drawing.Size(746, 316);
+            this.txtHistorial.TabIndex = 0;
+            this.txtHistorial.Text = "";
+            // 
+            // txtPregunta
+            // 
+            this.txtPregunta.Location = new System.Drawing.Point(12, 354);
+            this.txtPregunta.Name = "txtPregunta";
+            this.txtPregunta.Size = new System.Drawing.Size(424, 22);
+            this.txtPregunta.TabIndex = 1;
+            this.txtPregunta.TextChanged += new System.EventHandler(this.txtPregunta_TextChanged);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(489, 354);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(379, 416);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(44, 16);
+            this.lblEstado.TabIndex = 3;
+            this.lblEstado.Text = "label1";
+            // 
+            // FrmIA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtPregunta);
+            this.Controls.Add(this.txtHistorial);
+            this.Name = "FrmIA";
             this.Text = "FrmIA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtHistorial;
+        private System.Windows.Forms.TextBox txtPregunta;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblEstado;
     }
 }
--- GUI/FrmIA.cs
+++ GUI/FrmIA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Modelo_IA;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,52 @@ using System.Windows.Forms;
 
 namespace ProyectoP3
 {
+    private readonly AdministradorDeConversacion cm = new AdministradorDeConversacion();
+    private readonly LLMClient llm = new LLMClient();
     public partial class FrmIA : Form
     {
         public FrmIA()
         {
             InitializeComponent();
         }
+
+        private void txtPregunta_TextChanged(object sender, EventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Evita salto de línea
+                btnEnviar.PerformClick();
+            }
+        }
+
+        private async void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string userInput = txtPregunta.Text.Trim();
+            if (string.IsNullOrEmpty(userInput)) return;
+
+            txtHistorial.AppendText($"Tú: {userInput}\n");
+            txtPregunta.Clear();
+            lblEstado.Text = "Consultando a la IA...";
+
+            // Guardar mensaje del usuario
+            cm.AddMessage(Role.User, userInput);
+
+            try
+            {
+                var prompt = GenerarMensajes.BuildPrompt(cm.GetHistory(), userInput);
+                var respuesta = await llm.GetResponseAsync(prompt);
+
+                cm.AddMessage(Role.Assistant, respuesta);
+                txtHistorial.AppendText($"IA: {respuesta}\n\n");
+            }
+            catch (Exception ex)
+            {
+                txtHistorial.AppendText($"[Error: {ex.Message}]\n");
+            }
+
+            lblEstado.Text = "Listo";
+        }
+
     }
 }
+
